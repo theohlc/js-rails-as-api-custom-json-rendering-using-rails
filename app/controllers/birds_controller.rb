8e6b26c922c3494494486106d4655b2640620221
theo <teddyhoover@gmail.com>
2021-03-25 13:59:06 -0500

    commit tree bf23e44a25f9e6cfc5451ca26bcde525f7a29429

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: @birds
+    render json: @birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find(params[:id])
+    render json: bird.slice(:id, :name, :species)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get '/birds' => 'birds#index'
+  #get '/birds' => 'birds#index'
+  #get '/birds/:id' => 'birds#show'
+  resources: :birds, only: [ :index, :show ]
 end
